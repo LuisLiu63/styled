8d563bc124ec547e53ebc4a2faaa191171fa0d15
LuisLiu <lc63msn@hotmail.com>
2022-05-14 18:01:07 +0800

    [init]:add rollup & jsdoc

---
+++ rollup.config.dev.js
@@ -0,0 +1,7 @@
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: './dist/css-var.js',
+    format: 'cjs'
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Author: luisliu
+ * Description: css-var
+ * Time: 2022-05-14 17:37:29
+ * Contact: lc63msn@Hotmail.com
+ */
+
+export default {
+
+}
